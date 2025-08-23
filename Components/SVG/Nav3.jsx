83e17e9c9616908818bb614bbbf5c893f9e8fa12
Divyansh Patel <pateldivyansh131@gmail.com>
2025-08-23 23:29:43 +0530

    Final

--- Components/SVG/Nav3.jsx
+++ Components/SVG/Nav3.jsx
@@ -4,24 +4,22 @@ const Nav3 = () => {
   return (
     <svg
       className="brand-title"
-      width={180}
-      height={40}
+      width={200}
+      height={60}
       viewBox="0 0 180 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-
-      <path
-        d="M5 10h20v5h-7.5V30h-5V15H5v-5Z
-           M32 10h5v10.2L45 10h6l-9 9.5 9.5 10.5h-6L37 20.5V30h-5V10Z
-           M60 10h5v20h-5V10Z
-           M73 10h5v15h9v5h-14V10Z
-           M96 10h5v20h-5V10Z
-           M109 10h5v8h8v-8h5v20h-5v-7h-8v7h-5V10Z
-           M136 10h6l4 13 4-13h6l-7 20h-6l-7-20Z
-           M169 10h5v20h-5V10Z"
-        fill="#36C95F"
-      />
+      <text
+        x="10"
+        y="25"
+        fontFamily="Arial, sans-serif"
+        fontSize="35"
+        fontWeight="bold"
+        fill="#3D9662"
+      >
+        TrustMed
+      </text>
     </svg>
   );
 };
