5f0897b43794acca1b69ba2683aa05f19d25bd64
Divyansh Patel <pateldivyansh131@gmail.com>
2026-09-26 15:14:51 +0530

    updates in scripts & global css

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 
 //npx hardhat run scripts/deploy.js --network localhost
-//npx hardhat run scripts/deploy.js --network holesky
+//npx hardhat run scripts/deploy.js --network sepolia
 const hre = require("hardhat");
 
 async function main() {
